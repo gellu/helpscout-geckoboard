da0a4f81015d62f98c630da4122c662a2d0e1098
gellu <grkrysiak@gmail.com>
2015-10-15 15:24:26 +0200

    cron script

--- cron.sh
+++ cron.sh
@@ -2,6 +2,6 @@
 
 PHP="/usr/bin/env php"
 
-cd /var/www//var/www/helpscout-geckoboard.docplanner.io/app/
+cd /var/www/helpscout-geckoboard.docplanner.io/app/
 
 ${PHP} command.php log-assigned-to-fo-by-age
